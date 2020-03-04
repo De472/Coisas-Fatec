e50d1d483fbf1ab4a65caddf29bd9971c859eb44
Devanir Ramos Junior <junior472399@gmail.com>
2020-03-04 03:31:09 -0300

    Introdução

---
+++ Tabela_verdade.py
@@ -0,0 +1,19 @@
+
+print("Olá, aqui você pode ter tabelas verdade")
+print("feitas automátimaticamente!!\n")
+
+print("Instruções:")
+print("Basta apenas digitar quantos números/")
+print("vareáveis a tabela terá e a operação")
+print("lógica que será realizada.")
+print("Só é possível fazer no máximo 8 linhas")
+print("(no máximo 3 variáveis) e uma operação")
+print("por vez\n")
+
+começar = input("Digite qualquer coisa para começar\n")
+
+tamanho = input("Quantas ")
+
+
+
+
